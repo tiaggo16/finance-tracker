e0f8c3a878e66a30816822bc62a4da42d4df6c2f
tabosa <tiaggo95@hotmail.com>
2021-05-22 00:53:22 -0300

    add search stock form

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_token],
+                                  secret_token: Rails.application.credentials.iex_client[:sandbox_api_secret],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
 
 end

05d908caaaf545ef08f66010d177f2c64d5e730a
tabosa <tiaggo95@hotmail.com>
2021-06-13 21:13:28 -0300

    configure navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
   root 'welcome#index'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'

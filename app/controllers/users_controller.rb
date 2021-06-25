7b87df7eb624f6f2793e90d18a8f9a02e080559b
tabosa <tiaggo95@hotmail.com>
2021-06-25 18:38:13 -0300

    block user page for current_user and redirect to my portfolio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tracked_stocks = @user.stocks
+    if current_user != @user
+      @tracked_stocks = @user.stocks
+    else
+      redirect_to my_portfolio_path
+    end
   end
 
   def search
